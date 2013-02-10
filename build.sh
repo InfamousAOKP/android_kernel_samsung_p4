4088b5b7d3562c59b2a9663b3957f36c9692911c
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-02-10 11:54:46 +0100

    Update build scripts

--- build.sh
+++ build.sh
@@ -3,30 +3,37 @@
 
 # Build Mackay kernel for p4wifi
 
+# Set basic parameters
 DATE_START=$(date +"%s")
 
 MACKAY_VER="Mackay_1.0"
 
 export ARCH=arm
-export name
 export LOCALVERSION="-"`echo $MACKAY_VER`
 export CROSS_COMPILE=/home/kasper/android/cm101/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
-KERNEL_DIR=`pwd`
-OUTPUT_DIR=../output
+BASE_DIR=/home/kasper/android/cm101/kernel/samsung
+KERNEL_DIR=`echo $BASE_DIR`/p4_stock
+OUTPUT_DIR=`echo $BASE_DIR`/output
+KERNEL_DIR=`echo $OUTPUT_DIR`/kernel
 MODULES_DIR=`echo $OUTPUT_DIR`/modules
 
+# Clean
+rm $KERNEL_DIR/zImage
+rm $OUTPUT_DIR/*.zip
+
+# Build
 make mackay_p4wifi_defconfig
+make -j4
 
-make modules
-
-rm `echo $MODULES_DIR"/*"`
+# Move modules and kernel to output folder
+# rm `echo $MODULES_DIR"/*"`
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
-chmod 644 `echo $MODULES_DIR"/*"`
+# chmod 644 `echo $MODULES_DIR"/*"`
 
-make zImage
-mv arch/arm/boot/zImage $OUTPUT_DIR/kernel
+cp arch/arm/boot/zImage $OUTPUT_DIR/kernel
 
+Create flashable zip
 cd $OUTPUT_DIR
 zip -r `echo $MACKAY_VER`.zip *
 
