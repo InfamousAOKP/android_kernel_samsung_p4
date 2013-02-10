ed7f45ef07dbb1d7935daf9655361735f4ef844c
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-02-10 14:39:27 +0100

    Fix some erros in script and defconfig

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export LOCALVERSION="-"`echo $MACKAY_VER`
 export CROSS_COMPILE=/home/kasper/android/cm101/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
 BASE_DIR=/home/kasper/android/cm101/kernel/samsung
-KERNEL_DIR=`echo $BASE_DIR`/p4_stock
+SOURCE_DIR=`echo $BASE_DIR`/p4_stock
 OUTPUT_DIR=`echo $BASE_DIR`/output
 KERNEL_DIR=`echo $OUTPUT_DIR`/kernel
 MODULES_DIR=`echo $OUTPUT_DIR`/modules
@@ -23,19 +23,22 @@ rm $KERNEL_DIR/zImage
 rm $OUTPUT_DIR/*.zip
 
 # Build
+cd $SOURCE_DIR
 make mackay_p4wifi_defconfig
 make -j4
 
 # Move modules and kernel to output folder
 # rm `echo $MODULES_DIR"/*"`
-find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
-# chmod 644 `echo $MODULES_DIR"/*"`
 
-cp arch/arm/boot/zImage $OUTPUT_DIR/kernel
+find $SOURCE_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+chmod 644 `echo $MODULES_DIR"/*"`
 
-Create flashable zip
+cp arch/arm/boot/zImage $KERNEL_DIR
+
+# Create flashable zip
 cd $OUTPUT_DIR
 zip -r `echo $MACKAY_VER`.zip *
+cp `echo $MACKAY_VER`.zip /home/kasper/AndroidFlash/p4wifi/stock_kernel
 
 DATE_END=$(date +"%s")
 echo
