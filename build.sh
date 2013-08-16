679ea348109321c51fef18d53c4d17db04383e8d
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-08-16 16:23:46 +0200

    Prepare for separate kernel building
    Revert "Enable SELinux"
    Update buildscript and Makefile

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+
+# Build Infamous kernel for p4wifi
+
+# Set basic parameters
+DATE_START=$(date +"%s")
+
+INFAMOUS_VER="Infamous_JB43_noSE_GCC46_1.0"
+
+export ARCH=arm
+export LOCALVERSION="-"`echo $INFAMOUS_VER`
+# export CROSS_COMPILE=/home/kasper/android/cm102/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export CROSS_COMPILE=/home/kasper/android/cm102/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+
+
+BASE_DIR=/home/kasper/android/cm102/kernel/samsung
+SOURCE_DIR=`echo $BASE_DIR`/p4
+OUTPUT_DIR=`echo $BASE_DIR`/output/galaxytab
+KERNEL_DIR=`echo $OUTPUT_DIR`/kernel
+MODULES_DIR=`echo $OUTPUT_DIR`/modules
+
+# Clean
+rm $KERNEL_DIR/zImage
+rm $OUTPUT_DIR/*.zip
+
+# Build
+cd $SOURCE_DIR
+make infamous_p4wifi_defconfig
+make -j4
+
+# Move modules and kernel to output folder
+# rm `echo $MODULES_DIR"/*"`
+
+find $SOURCE_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+chmod 644 `echo $MODULES_DIR"/*"`
+
+cp arch/arm/boot/zImage $KERNEL_DIR
+
+# Create flashable zip
+cd $OUTPUT_DIR
+zip -r `echo $INFAMOUS_VER`.zip *
+
+DATE_END=$(date +"%s")
+echo
+DIFF=$(($DATE_END - $DATE_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (Infamous_kernel_2.4)"
+	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
